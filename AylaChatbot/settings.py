0a4dd036227064743b7c4ca14ba42bcdf3d41a80
[Jessica Chioma] <[augusthottie@icloud.com]>
2023-08-03 22:27:24 +0200

    removed authentication backends

--- AylaChatbot/settings.py
+++ AylaChatbot/settings.py
@@ -103,13 +103,13 @@ DATABASES = {
     }
 }
 
-AUTHENTICATION_BACKENDS = [
-    # Needed to login by username in Django admin, regardless of `allauth`
-    'django.contrib.auth.backends.ModelBackend',
+# AUTHENTICATION_BACKENDS = [
+#     # Needed to login by username in Django admin, regardless of `allauth`
+#     'django.contrib.auth.backends.ModelBackend',
 
-    # `allauth` specific authentication methods, such as login by e-mail
-    'allauth.account.auth_backends.AuthenticationBackend',
-]
+#     # `allauth` specific authentication methods, such as login by e-mail
+#     # 'allauth.account.auth_backends.AuthenticationBackend',
+# ]
 
 
 # Password validation
--- AylaChatbot/urls.py
+++ AylaChatbot/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+    # path('accounts/', include('allauth.urls')),
     path('', include('Ayla.urls')),
-    path('chatbot/', include('Ayla.urls')),
+    # path('chatbot/', include('Ayla.urls')),
 ]
