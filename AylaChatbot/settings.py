8b2fca8d2b3b3a35d6020a359533ce077eedb797
[Jessica Chioma] <[augusthottie@icloud.com]>
2023-08-06 05:21:21 +0200

    .env changes

--- AylaChatbot/settings.py
+++ AylaChatbot/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#5y(*a3m3cs)dl0rqz9z^821gay)c!wrkni1o4p&f^rz*^-uy('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
