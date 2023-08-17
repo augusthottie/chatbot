6fc7cd37cd29281a5f76ea00abeab9783723155d
[Jessica Chioma] <[augusthottie@icloud.com]>
2023-08-17 19:37:16 +0200

    added hosts

--- AylaChatbot/settings.py
+++ AylaChatbot/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#5y(*a3m3cs)dl0rqz9z^821gay)c!wrkni1o4p&f^rz*^-uy(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.211.73.82', 'ec2-3-211-73-82.compute-1.amazonaws.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
