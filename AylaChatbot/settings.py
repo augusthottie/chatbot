719f20e396bb67bd68ade01e104cba3ba302bb3a
[Jessica Chioma] <[augusthottie@icloud.com]>
2023-08-17 19:30:39 +0200

    changes

--- AylaChatbot/settings.py
+++ AylaChatbot/settings.py
@@ -96,23 +96,23 @@ WSGI_APPLICATION = 'AylaChatbot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ayla', 
-        'USER': 'admin',
-        'PASSWORD': 'Chioma@23',
-        'HOST': 'localhost', 
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'ayla', 
+#         'USER': 'admin',
+#         'PASSWORD': 'Chioma@23',
+#         'HOST': 'localhost', 
+#         'PORT': '',
+#     }
+# }
 
 # AUTHENTICATION_BACKENDS = [
 #     # Needed to login by username in Django admin, regardless of `allauth`
