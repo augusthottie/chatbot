c89469d5a66baa215640e4f173081edabc3783f8
[Jessica Chioma] <[augusthottie@icloud.com]>
2023-08-17 19:26:47 +0200

    postgresql

--- AylaChatbot/settings.py
+++ AylaChatbot/settings.py
@@ -96,10 +96,21 @@ WSGI_APPLICATION = 'AylaChatbot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ayla', 
+        'USER': 'admin',
+        'PASSWORD': 'Chioma@23',
+        'HOST': 'localhost', 
+        'PORT': '',
     }
 }
 
